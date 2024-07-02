9be7bec656f5d519d24d252ff9b81c5c6681babb
NedFIander <kuldeep1702k@gmail.com>
2024-07-02 18:59:28 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,15 @@
 from nada_dsl import *
 
+
 def nada_main():
     party1 = Party(name="Party1")
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    # Perform an arithmetic operation (addition) on the secret integers
-    result = my_int1 % my_int2
+    # Compute the sum of my_int1 and my_int2
+    sum_result = my_int1 + my_int2
 
-    # Return the result as the output
-    return [Output(result, "Modulus_output",party1)]
+    # Define the output
+    output = Output(sum_result, "sum_output", party1)
+
+    return [output]
